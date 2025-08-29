fc2e1acc9fc52316a8be22b92b8f93e09b1441c2
lopescmdea <76059079+lopescmdea@users.noreply.github.com>
2025-08-29 19:52:07 -0300

    290820251952

--- 03_build_report_models.r
+++ 03_build_report_models.r
@@ -12,22 +12,22 @@ model_data <- readxl::read_excel(path = 'data/model_data/model_data.xlsx')
 #TERMAQ_per_trab
 #PROP_per_trab
 
-mod0 <- plm(productivitye ~ BEN_per_trab + SAL_per_trab + PREVSOC_per_trab + PREVPRI_per_trab + INDENIZ_per_trab + TER_per_trab +TERMAQ_per_trab + PROP_per_trab, 
+mod0 <- plm(productivitye ~ BEN_per_trab + SAL_per_trab + PREVSOC_per_trab + INDENIZ_per_trab + TER_per_trab +TERMAQ_per_trab, 
             data= model_data,
             model="within", 
             effect="twoways")
 
 print(summary(mod0))
 
-mod1 <- plm(INDENIZ_per_trab ~ PROP_per_trab, 
+mod1 <- plm(BEN_per_trab ~ PROP_per_trab + PREVPRI_per_trab , 
             data = model_data, 
             model="within", 
             effect="twoways")
 
 summary(mod1)
 
-mod2 <- plm(productivitye ~ BEN_per_trab + SAL_per_trab + PREVSOC_per_trab + PREVPRI_per_trab + INDENIZ_per_trab + TER_per_trab +TERMAQ_per_trab | # VARIAÇÃO NO TRABALHO EM T-1
-            SAL_per_trab + PREVSOC_per_trab + PREVPRI_per_trab + INDENIZ_per_trab + TER_per_trab +TERMAQ_per_trab + PROP_per_trab, # SERVIRÇOS PRESTADOS POR TERCEIROS EM T-1 + SERVIÇOS DE REPARO E MANUTENÇÃO PRESTADOR POR TERCEIROS EM T-1
+mod2 <- plm(productivitye ~ BEN_per_trab + SAL_per_trab + PREVSOC_per_trab + INDENIZ_per_trab + TER_per_trab +TERMAQ_per_trab | 
+            SAL_per_trab + PREVSOC_per_trab + PREVPRI_per_trab + INDENIZ_per_trab + TER_per_trab +TERMAQ_per_trab + PROP_per_trab, 
             data = model_data, 
             model= "within", 
             effect="twoways")
