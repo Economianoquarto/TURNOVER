1444d6ff5c9e8ca1ae7aae15bc28f2ec8302fb40
lopescmdea <76059079+lopescmdea@users.noreply.github.com>
2025-09-07 17:40:09 -0300

    des07092025

--- 03_build_report_models.r
+++ 03_build_report_models.r
@@ -81,137 +81,183 @@ esdesc <- esdesc %>%
 
 write_xlsx(esdesc, path = "data/model_data/esdesc.xlsx")
 
-##########################
-######### PREV.SOC########
-##########################
+# Lista das variáveis dependentes
+dependent_vars <- c("PREVSOC_per_trab", "SAL_per_trab", "BEN_per_trab", 
+                    "TER", "INDENIZ_per_trab")
 
-# twfe model 
-twfe_PREV.SOC <- lm(PREVSOC_per_trab ~ finalpolicy + 
-                      factor(cnaeidnum) + factor(Ano),
-                    data = diddf)
+# Lista para armazenar todos os resultados
+all_results <- list()
 
-excel_twfe_PREV.SOC <- write_xlsx(tidy(twfe_PREV.SOC), path = "data/model_data/twfe_PREV.SOC.xlsx") 
-
-##### Event Study ######
-
-# run fe ols 
-twfe_PREV.SOC = feols(PREVSOC_per_trab ~ i(time_to_treat, treated, ref = -1)|
-                        factor(cnaeidnum) + factor(Ano),vcov_cluster, data = diddf)
-
-tidy_twfe_PREV.SOC = tidy(twfe_PREV.SOC, conf.int = TRUE)
-
-tidy_twfe_PREV.SOC <- tidy_twfe_PREV.SOC %>%
-  mutate(term = ifelse(term == 'time_to_treat::-6:treated', -6, 
-                       ifelse(term == 'time_to_treat::-5:treated', -5,
-                              ifelse(term == 'time_to_treat::-4:treated', -4,
-                                     ifelse(term == 'time_to_treat::-3:treated', -3,
-                                            ifelse(term == 'time_to_treat::-2:treated', -2, 
-                                                   ifelse(term == 'time_to_treat::-1:treated', -1,
-                                                          ifelse(term == 'time_to_treat::0:treated', 0,
-                                                                 ifelse(term == 'time_to_treat::1:treated', 1,
-                                                                        ifelse(term == 'time_to_treat::2:treated', 2, 
-                                                                               3)))))))))) 
-
-tidy_twfe_PREV.SOC <- tidy_twfe_PREV.SOC %>% 
-  dplyr::select(term,estimate,std.error,conf.low,conf.high) %>%
-  mutate(approach = "TWFE") 
-
-# now run Sun and Abraham method model 
-sa_PREV.SOC = feols(PREVSOC_per_trab ~ sunab(year_treated, Ano,ref.p=-1) | factor(cnaeidnum) + factor(Ano),
-                    vcov_cluster, 
-                    data = diddf)
-
-tidy_sa_PREV.SOC = tidy(sa_PREV.SOC,conf.int = TRUE)
-
-tidy_sa_PREV.SOC <- tidy_sa_PREV.SOC %>%
-  mutate(term = ifelse(term == 'Ano::-6', -6, 
-                       ifelse(term == 'Ano::-5', -5,
-                              ifelse(term == 'Ano::-4', -4,
-                                     ifelse(term == 'Ano::-3', -3,
-                                            ifelse(term == 'Ano::-2', -2, 
-                                                   ifelse(term == 'Ano::-1', -1,
-                                                          ifelse(term == 'Ano::0', 0,
-                                                                 ifelse(term == 'Ano::1', 1,
-                                                                        ifelse(term == 'Ano::2', 2, 
-                                                                               3)))))))))) 
-
-tidy_sa_PREV.SOC <- tidy_sa_PREV.SOC %>% 
-  dplyr::select(term,estimate,std.error,conf.low,conf.high) %>%
-  mutate(approach = "Sun & Abraham")    
-
-# Santanna & Callaway plot with event-study
-csa_PREV.SOC <- att_gt(yname = "PREVSOC_per_trab",
-                       gname = "first_treat",
-                       idname = "cnaeidnum",
-                       tname = "Ano",
-                       xformla = ~ 1,
+# Loop para cada variável dependente
+for (var in dependent_vars) {
+  
+  cat("Processando variável:", var, "\n")
+  
+  # Criar fórmula dinâmica
+  formula_twfe <- as.formula(paste(var, "~ finalpolicy + factor(cnaeidnum) + factor(Ano)"))
+  formula_feols <- as.formula(paste(var, "~ i(time_to_treat, treated, ref = -1) | factor(cnaeidnum) + factor(Ano)"))
+  formula_sa <- as.formula(paste(var, "~ sunab(year_treated, Ano, ref.p=-1) | factor(cnaeidnum) + factor(Ano)"))
+  
+  # 1. TWFE model 
+  twfe_model <- lm(formula_twfe, data = diddf)
+  
+  # Salvar resultado TWFE
+  write_xlsx(tidy(twfe_model), path = paste0("data/model_data/twfe_", var, ".xlsx"))
+  
+  # 2. Event Study TWFE
+  twfe_es <- feols(formula_feols, vcov_cluster, data = diddf)
+  tidy_twfe <- tidy(twfe_es, conf.int = TRUE)
+  
+  tidy_twfe <- tidy_twfe %>%
+    mutate(term = ifelse(term == 'time_to_treat::-6:treated', -6, 
+                         ifelse(term == 'time_to_treat::-5:treated', -5,
+                                ifelse(term == 'time_to_treat::-4:treated', -4,
+                                       ifelse(term == 'time_to_treat::-3:treated', -3,
+                                              ifelse(term == 'time_to_treat::-2:treated', -2, 
+                                                     ifelse(term == 'time_to_treat::-1:treated', -1,
+                                                            ifelse(term == 'time_to_treat::0:treated', 0,
+                                                                   ifelse(term == 'time_to_treat::1:treated', 1,
+                                                                          ifelse(term == 'time_to_treat::2:treated', 2, 
+                                                                                 3)))))))))) 
+  
+  tidy_twfe <- tidy_twfe %>% 
+    dplyr::select(term, estimate, std.error, conf.low, conf.high) %>%
+    mutate(approach = "TWFE", variable = var)
+  
+  # 3. Sun and Abraham method
+  sa_model <- feols(formula_sa, vcov_cluster, data = diddf)
+  tidy_sa <- tidy(sa_model, conf.int = TRUE)
+  
+  tidy_sa <- tidy_sa %>%
+    mutate(term = ifelse(term == 'Ano::-6', -6, 
+                         ifelse(term == 'Ano::-5', -5,
+                                ifelse(term == 'Ano::-4', -4,
+                                       ifelse(term == 'Ano::-3', -3,
+                                              ifelse(term == 'Ano::-2', -2, 
+                                                     ifelse(term == 'Ano::-1', -1,
+                                                            ifelse(term == 'Ano::0', 0,
+                                                                   ifelse(term == 'Ano::1', 1,
+                                                                          ifelse(term == 'Ano::2', 2, 
+                                                                                 3)))))))))) 
+  
+  tidy_sa <- tidy_sa %>% 
+    dplyr::select(term, estimate, std.error, conf.low, conf.high) %>%
+    mutate(approach = "Sun & Abraham", variable = var)
+  
+  # 4. Callaway & Sant'anna
+  csa_model <- att_gt(yname = var,
+                      gname = "first_treat",
+                      idname = "cnaeidnum",
+                      tname = "Ano",
+                      xformla = ~ 1,
+                      data = diddf,
+                      est_method = "dr",
+                      control_group = "nevertreated",
+                      bstrap = T,
+                      base_period = "universal",
+                      allow_unbalanced_panel = T)
+  
+  csa_es <- aggte(csa_model, type = "dynamic")
+  tidy_csa <- tidy(csa_es)
+  
+  tidy_csa <- tidy_csa %>%
+    mutate(term = ifelse(term == 'ATT(-6)', -6, 
+                         ifelse(term == 'ATT(-5)', -5,
+                                ifelse(term == 'ATT(-4)', -4,
+                                       ifelse(term == 'ATT(-3)', -3,
+                                              ifelse(term == 'ATT(-2)', -2, 
+                                                     ifelse(term == 'ATT(-1)', -1,
+                                                            ifelse(term == 'ATT(0)', 0,
+                                                                   ifelse(term == 'ATT(1)', 1,
+                                                                          ifelse(term == 'ATT(2)', 2, 
+                                                                                 3)))))))))) 
+  
+  tidy_csa <- tidy_csa %>% 
+    dplyr::select(term, estimate, std.error, conf.low, conf.high) %>%
+    mutate(approach = "Callaway & Sant'anna", variable = var)
+  
+  # 5. ETWFE
+  etwfe_model <- etwfe(fml = as.formula(paste(var, "~ 1")),
+                       tvar = Ano,
+                       gvar = first_treat,
                        data = diddf,
-                       est_method = "dr",
-                       control_group = "nevertreated",
-                       bstrap=T,
-                       base_period = "universal",
-                       allow_unbalanced_panel = T
-)
+                       vcov = ~cnaeidnum,
+                       cgroup = "never")
+  
+  etwfe_es <- emfx(etwfe_model, type = "event")
+  tidy_etwfe <- tidy(etwfe_es)
+  
+  tidy_etwfe <- tidy_etwfe %>%
+    select(-term) %>%
+    rename(term = event)
+  
+  tidy_etwfe <- tidy_etwfe %>% 
+    dplyr::select(term, estimate, std.error, conf.low, conf.high) %>%
+    mutate(approach = "ETWFE", variable = var)
+  
+  # Combinar todos os resultados para esta variável
+  var_results <- rbind(tidy_twfe, tidy_sa, tidy_csa, tidy_etwfe)
+  
+  # Adicionar à lista de todos os resultados
+  all_results[[var]] <- var_results
+  
+  # Criar gráfico para esta variável
+  dodge <- position_dodge(width = 0.3)
+  
+  p <- ggplot(var_results, aes(x = term,
+                               y = estimate,
+                               ymin = conf.low,
+                               ymax = conf.high,
+                               color = approach,
+                               shape = approach)) +
+    geom_pointrange(position = dodge) +
+    geom_hline(yintercept = 0, color = "black") +
+    geom_vline(xintercept = 0, linetype = "dashed", color = "red") +
+    labs(x = "Time to treatment",
+         y = "ATT",
+         title = paste("Group-Time ATTs por abordagem -", var)) +
+    facet_wrap(~approach) +  
+    theme_minimal()
+  
+  # Salvar gráfico
+  
+  ggsave(file.path("figs", paste0("res_", var, ".png")), plot = p, width = 10, height = 6)
+  
+   
+  cat("Concluído para variável:", var, "\n\n")
+}
 
-csa_es_PREV.SOC <- aggte(csa_PREV.SOC, type = "dynamic")
-tidy_csaes_PREV.SOC = tidy(csa_es_PREV.SOC)
+# Combinar todos os resultados em um único dataframe
+final_results <- do.call(rbind, all_results)
 
-tidy_csaes_PREV.SOC <- tidy_csaes_PREV.SOC %>%
-  mutate(term = ifelse(term == 'ATT(-6)', -6, 
-                       ifelse(term == 'ATT(-5)', -5,
-                              ifelse(term == 'ATT(-4)', -4,
-                                     ifelse(term == 'ATT(-3)', -3,
-                                            ifelse(term == 'ATT(-2)', -2, 
-                                                   ifelse(term == 'ATT(-1)', -1,
-                                                          ifelse(term == 'ATT(0)', 0,
-                                                                 ifelse(term == 'ATT(1)', 1,
-                                                                        ifelse(term == 'ATT(2)', 2, 
-                                                                               3)))))))))) 
+# Salvar resultados combinados
+write_xlsx(final_results, path = "data/model_data/all_estimators_results.xlsx")
 
-tidy_csaes_PREV.SOC <- tidy_csaes_PREV.SOC %>% 
-  dplyr::select(term,estimate,std.error,conf.low,conf.high) %>%
-  mutate(approach = "Callaway & Sant'anna")
-
-# ETWFE plot with event-study
-etwfe_PREV.SOC <- etwfe(fml  = PREVSOC_per_trab ~1, # outcome ~ controls
-                        tvar = Ano,        # time variable
-                        gvar = first_treat, # group variable
-                        data = diddf,       # dataset
-                        vcov = ~cnaeidnum,  # vcov adjustment
-                        cgroup = "never"
-                        #cgroup = "notyet"
-)
-etwfees_PREV.SOC <- emfx(etwfe_PREV.SOC, type = "event")
-tidy_etwfees_PREV.SOC = tidy(etwfees_PREV.SOC)
-
-tidy_etwfees_PREV.SOC <- tidy_etwfees_PREV.SOC %>%
-  select(-term) %>%
-  rename(term = event)
-
-tidy_etwfees_PREV.SOC <- tidy_etwfees_PREV.SOC %>% 
-  dplyr::select(term,estimate,std.error,conf.low,conf.high) %>%
-  mutate(approach = "ETWFE")
-
-############
-res_PREV.SOC <- rbind(tidy_twfe_PREV.SOC, tidy_sa_PREV.SOC,tidy_csaes_PREV.SOC,tidy_etwfees_PREV.SOC)
-
-dodge <- position_dodge(width=0.3)
-
-# Criando o gráfico com facets para cada abordagem
-ggplot(res_PREV.SOC, aes(x = term,
-                         y = estimate,
-                         ymin = conf.low,
-                         ymax = conf.high,
-                         color = approach,
-                         shape = approach)) +
-  geom_pointrange(position = dodge) +
+# Criar gráfico comparativo com todas as variáveis
+comparison_plot <- ggplot(final_results, aes(x = term,
+                                             y = estimate,
+                                             ymin = conf.low,
+                                             ymax = conf.high,
+                                             color = approach,
+                                             shape = approach)) +
+  geom_pointrange(position = position_dodge(width = 0.3)) +
   geom_hline(yintercept = 0, color = "black") +
   geom_vline(xintercept = 0, linetype = "dashed", color = "red") +
   labs(x = "Time to treatment",
        y = "ATT",
-       title = "Group-Time ATTs por abordagem") +
-  facet_wrap(~approach) +  
-  theme_minimal()
+       title = "Comparação de ATTs por variável e abordagem") +
+  facet_grid(variable ~ approach, scales = "free_y") +  
+  theme_bw() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background = element_rect(fill = "white", color = NA),
+    strip.background = element_rect(fill = "white", color = "black"),
+    panel.grid.major = element_line(color = "grey90"),
+    panel.grid.minor = element_line(color = "grey95"),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
 
-# Salvando a figura
-ggsave("res_PREV.SOC.png", width = 10, height = 6)
+# Salvar gráfico comparativo
+ggsave(file.path("figs", "comparison_all_variables.png"), plot = comparison_plot, width = 16, height = 20, bg = "white")
+
+cat("Análise completa! Todos os resultados foram salvos.\n")
